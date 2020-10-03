241353833ca3281f5c01bddbaa32b49dbc848511
Quang Phan <quangpn@dgroup.co>
2020-10-03 22:01:43 +0700

    chore: create config.fish

---
+++ .fish/config.fish
@@ -0,0 +1,11 @@
+# Tide config
+set -U tide_git_prompt_bg_color 1e4659
+set -U tide_nvm_bg_color  2a5e53
+set -U tide_nvm_color  32CD32
+
+set -U tide_left_prompt_items  context\x1epwd\x1egit_prompt\x1enewline\x1eprompt_char
+set -U tide_right_prompt_items  status\x1ecmd_duration\x1ejobs\x1envm\x1erust\x1etime
+
+set -U tide_right_prompt_suffix ''
+set -U tide_prompt_char_bg_color ''
+set -U tide_left_prompt_prefix ''
