4ad39c7f8d4cad45d1f104af2050414208a02cc0
Quang Phan <contact.quangphan@gmail.com>
2021-11-06 00:03:30 +0700

    fix(fish): fix vi mode key bindings

--- .fish/functions/fish_user_key_bindings.fish
+++ .fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,10 @@
 function fish_user_key_bindings
-  fish_vi_key_bindings
+  fish_default_key_bindings -M insert
+
+  # Then execute the vi-bindings so they take precedence when there's a conflict.
+  # Without --no-erase fish_vi_key_bindings will default to
+  # resetting all bindings.
+  # The argument specifies the initial mode (insert, "default" or visual).
+  fish_vi_key_bindings --no-erase insert
   bind -M insert -m default jj backward-char force-repaint
 end
