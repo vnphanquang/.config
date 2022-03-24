ba01348ff0e894ccb5dfc4cd79c1e2940276f54b
Quang Phan <vnphanquang@gmail.com>
2022-03-24 17:14:30 +0700

    chore(fish): upgrade & add fzf plugin through fisher

--- .fish/config.fish
+++ .fish/config.fish
@@ -1,5 +1,4 @@
 # Tide config
-
 set -U tide_git_bg_color 1e4659
 set -U tide_node_bg_color 2a5e53
 set -U tide_node_color 32CD32
@@ -11,20 +10,15 @@ set -U tide_right_prompt_items status\x1ecmd_duration\x1econtext\x1ejobs\x1enode
 # set -U tide_prompt_char_bg_color ''
 # set -U tide_left_prompt_prefix ''
 
+# Keybinding
 set -U EDITOR vim
-
 set fish_key_bindings fish_user_key_bindings
-
-# tabtab source for electron-forge package
-# uninstall by removing these lines or running `tabtab uninstall electron-forge`
-[ -f /Users/vnphanquang/dev/dsv/odc/apps/compagnon/uninstaller/node_modules/tabtab/.completions/electron-forge.fish ]; and . /Users/vnphanquang/dev/dsv/odc/apps/compagnon/uninstaller/node_modules/tabtab/.completions/electron-forge.fish
 
 # direnv hook
 direnv hook fish | source
 set -g direnv_fish_mode eval_on_arrow
 
 # OS specific settings
-
 switch (uname)
   case Darwin
     set -gx PATH "$HOME/.cargo/bin" (go env GOPATH)/bin $PATH;
