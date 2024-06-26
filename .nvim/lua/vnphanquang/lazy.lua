8421f5b94ef36f7727687427e3a01688769e3d5a
Quang Phan <vnphanquang@gmail.com>
2024-06-26 19:06:56 +0700

    chore(nvim): declare plugins in spec array

--- .nvim/lua/vnphanquang/lazy.lua
+++ .nvim/lua/vnphanquang/lazy.lua
@@ -13,60 +13,60 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	-- base helpers
-	require("vnphanquang.plugins.which-key"),
-	require("vnphanquang.plugins.notify"),
-	{ "tpope/vim-sleuth" }, -- https://github.com/tpope/vim-sleuth
+	spec = {
+		require("vnphanquang.plugins.which-key"),
+		require("vnphanquang.plugins.notify"),
 
-	-- UI enhancement
-	require("vnphanquang.plugins.gruvbox-material"),
-	{ "mhinz/vim-startify" }, -- https://github.com/mhinz/vim-startify
-	-- https://github.com/xiyaowong/transparent.nvim
-	-- runs ":TransparentEnable", result will be cached
-	{ "xiyaowong/transparent.nvim" },
-	require("vnphanquang.plugins.lualine"),
+		-- UI enhancement
+		require("vnphanquang.plugins.gruvbox-material"),
+		{ "mhinz/vim-startify" }, -- https://github.com/mhinz/vim-startify
+		-- https://github.com/xiyaowong/transparent.nvim
+		-- runs ":TransparentEnable", result will be cached
+		{ "xiyaowong/transparent.nvim" },
+		require("vnphanquang.plugins.lualine"),
 
-	-- Workflow enhancement
-	require("vnphanquang.plugins.neo-tree"),
-	require("vnphanquang.plugins.markdown-preview"),
-	require("vnphanquang.plugins.treesitter"),
-	require("vnphanquang.plugins.telescope"),
-	require("vnphanquang.plugins.todo"),
-	require("vnphanquang.plugins.undotree"),
-	require("vnphanquang.plugins.spider"),
-	-- https://github.com/andrewferrier/wrapping.nvim
-	{
-		"andrewferrier/wrapping.nvim",
-		config = function()
-			require("wrapping").setup({
-				notify_on_switch = false,
-			})
-			vim.api.nvim_create_autocmd({ 'BufEnter', 'BufNewFile' }, {
-				pattern = '*',
-				command = 'SoftWrapMode',
-			})
-		end,
+		-- Workflow enhancement
+		require("vnphanquang.plugins.neo-tree"),
+		require("vnphanquang.plugins.markdown-preview"),
+		require("vnphanquang.plugins.treesitter"),
+		require("vnphanquang.plugins.telescope"),
+		require("vnphanquang.plugins.todo"),
+		require("vnphanquang.plugins.undotree"),
+		require("vnphanquang.plugins.spider"),
+		-- https://github.com/andrewferrier/wrapping.nvim
+		{
+			"andrewferrier/wrapping.nvim",
+			config = function()
+				require("wrapping").setup({
+					notify_on_switch = false,
+				})
+				vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
+					pattern = "*",
+					command = "SoftWrapMode",
+				})
+			end,
+		},
+
+		-- essential development plugins
+		require("vnphanquang.plugins.git"),
+		require("vnphanquang.plugins.lsp"),
+		require("vnphanquang.plugins.lsp.typescript-tools"),
+		require("vnphanquang.plugins.conform"),
+		require("vnphanquang.plugins.trouble"),
+		{ "github/copilot.vim" }, -- https://github.com/github/copilot.vim
+
+		-- development utilities
+		require("vnphanquang.plugins.autopairs"),
+		require("vnphanquang.plugins.surround"),
+		require("vnphanquang.plugins.indent-blank-line"),
+		require("vnphanquang.plugins.hop"),
+		require("vnphanquang.plugins.illuminate"),
+		require("vnphanquang.plugins.substitue"),
+		require("vnphanquang.plugins.multiple-cursor"),
+		{ "numToStr/Comment.nvim", opts = {} }, -- https://github.com/numToStr/Comment.nvim
+
+		-- fun stuff
+		{ "eandrju/cellular-automaton.nvim" }, -- https://github.com/Eandrju/cellular-automaton.nvim
 	},
-
-	-- essential development plugins
-	require("vnphanquang.plugins.git"),
-	require("vnphanquang.plugins.lsp"),
-	require("vnphanquang.plugins.lsp.typescript-tools"),
-	require("vnphanquang.plugins.conform"),
-	require("vnphanquang.plugins.trouble"),
-	{ "github/copilot.vim" }, -- https://github.com/github/copilot.vim
-
-	-- development utilities
-	require("vnphanquang.plugins.autopairs"),
-	require("vnphanquang.plugins.surround"),
-	require("vnphanquang.plugins.indent-blank-line"),
-	require("vnphanquang.plugins.hop"),
-	require("vnphanquang.plugins.illuminate"),
-	require("vnphanquang.plugins.substitue"),
-	require("vnphanquang.plugins.multiple-cursor"),
-	{ "numToStr/Comment.nvim", opts = {} }, -- https://github.com/numToStr/Comment.nvim
-
-	-- fun stuff
-	{ "eandrju/cellular-automaton.nvim" }, -- https://github.com/Eandrju/cellular-automaton.nvim
 })
 
