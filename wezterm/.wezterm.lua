5130372cf70d5ff6fab0d4333863b66939874cd1
Quang Phan <vnphanquang@gmail.com>
2025-06-09 12:31:12 +0700

    chore(alacritty+wezterm): turn off title bar

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -16,6 +16,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font("FiraCode Nerd Font")
 config.color_scheme = "GruvboxDark"
 config.window_background_opacity = 0.9
+config.window_decorations = 'NONE'
 
 -- and finally, return the configuration to wezterm
 return config
