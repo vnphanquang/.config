ac395781a08dc6f59e49ee53fbe0aea0abc63650
Quang Phan <vnphanquang@gmail.com>
2025-07-03 15:23:35 +0700

    chore: misc

--- nvim/lua/vnphanquang/plugins/lsp.lua
+++ nvim/lua/vnphanquang/plugins/lsp.lua
@@ -289,7 +289,6 @@ return {
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 			require("mason-lspconfig").setup({
-				ensure_installed = ensure_installed,
 				handlers = {
 					function(server_name)
 						local server = servers[server_name] or {}
