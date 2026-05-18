1ace477244dda97bac14d76c097f28ee943ce82b
Quang Phan <vnphanquang@gmail.com>
2026-05-18 19:56:55 +0700

    chore(nvim): misc format

--- nvim/lua/vnphanquang/lsp/ecosystem/javascript.lua
+++ nvim/lua/vnphanquang/lsp/ecosystem/javascript.lua
@@ -3,41 +3,47 @@ local eco = require("vnphanquang.lsp.ecosystem.utils")
 
 return eco.define_lsp_tools({
 	{
-		mason_id = 'denols',
+		mason_id = "denols",
 		config = {
 			root_dir = function(bufnr, on_dir)
 				local deno_root = utils.get_deno_root(bufnr)
-				if deno_root then on_dir(deno_root) end
+				if deno_root then
+					on_dir(deno_root)
+				end
 			end,
-		}
+		},
 	},
 	"eslint-lsp",
 	"eslint_d",
 	"js-debug-adapter", -- used in conjunction with nvim-dap, see `dap.lua`
 	{
-		mason_id = 'stylelint-lsp',
+		mason_id = "stylelint_lsp",
 		config = {
 			root_dir = function(bufnr, on_dir)
 				local root = vim.fs.root(bufnr, function(filename)
 					return filename:match("^.prettierc") or filename:match("^prettier.config")
 				end)
-				if root then on_dir(root) end
+				if root then
+					on_dir(root)
+				end
 			end,
-		}
+		},
 	},
 	{
-		mason_id = 'stylelint-lsp',
+		mason_id = "stylelint_lsp",
 		config = {
 			root_dir = function(bufnr, on_dir)
 				local root = vim.fs.root(bufnr, function(filename)
 					return filename:match("^.prettierc") or filename:match("^prettier.config")
 				end)
-				if root then on_dir(root) end
+				if root then
+					on_dir(root)
+				end
 			end,
-		}
+		},
 	},
 	{
-		mason_id = 'svelte',
+		mason_id = "svelte",
 		config = {
 			on_attach = function(client)
 				-- https://github.com/sveltejs/language-tools/issues/2008
@@ -50,19 +56,30 @@ return eco.define_lsp_tools({
 					end,
 				})
 			end,
-		}
+		},
 	},
-	"tailwindcss",
 	{
-		mason_id = 'ts_ls',
+		mason_id = "tailwindcss",
+		config = {
+			settings = {
+				tailwindCSS = {
+					classFunctions = { "tw", "clsx" },
+				},
+			},
+		},
+	},
+	{
+		mason_id = "ts_ls",
 		config = {
 			root_dir = function(bufnr, on_dir)
 				local deno_root = utils.get_deno_root(bufnr)
 				if not deno_root then
-					local root = vim.fs.root(bufnr, { 'package.json', 'tsconfig.json', 'jsconfig.json', 'tsconfig.jsonc' })
-					if root then on_dir(root) end
+					local root = vim.fs.root(bufnr, { "package.json", "tsconfig.json", "jsconfig.json", "tsconfig.jsonc" })
+					if root then
+						on_dir(root)
+					end
 				end
 			end,
-		}
+		},
 	},
 })
