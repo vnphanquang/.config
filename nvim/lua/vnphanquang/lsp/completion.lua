d6e51c4e934663d5be077fd006e25733f4d68a58
Quang Phan <vnphanquang@gmail.com>
2026-06-23 17:52:11 +0700

    chore(linux): try blink.cmp for completion

--- nvim/lua/vnphanquang/lsp/completion.lua
+++ nvim/lua/vnphanquang/lsp/completion.lua
@@ -1,59 +1,30 @@
 return {
 	{
-		"rafamadriz/friendly-snippets",
-	},
-	{
-		"L3MON4D3/LuaSnip",
+		"saghen/blink.cmp",
 		dependencies = { "rafamadriz/friendly-snippets" },
-	},
-	{
-		"hrsh7th/cmp-nvim-lsp",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			{ "antosha417/nvim-lsp-file-operations", config = true },
-			{ "folke/lazydev.nvim" },
+		version = "1.*",
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			completion = {
+				documentation = { auto_show = true, auto_show_delay_ms = 500 },
+			},
+			keymap = {
+				preset = "none",
+				["<C-k>"] = { "show", "show_documentation", "hide_documentation" },
+				["<CR>"] = { "select_and_accept", "fallback" },
+				--
+				["<Up>"] = { "select_prev", "fallback" },
+				["<Down>"] = { "select_next", "fallback" },
+				--
+				["J"] = { "scroll_documentation_down", "fallback" },
+				["K"] = { "scroll_documentation_up", "fallback" },
+				--
+				["<Tab>"] = { "snippet_forward", "fallback" },
+				["<S-Tab>"] = { "snippet_backward", "fallback" },
+				--
+			},
 		},
-		config = function()
-			local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-			-- used to enable autocompletion (assign to every lsp server config)
-			local capabilities = cmp_nvim_lsp.default_capabilities()
-
-			vim.lsp.config("*", { capabilities = capabilities })
-		end,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"L3MON4D3/LuaSnip",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-					["<C-k>"] = cmp.mapping.complete(),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
+		opts_extend = { "sources.default" },
 	},
 }
